1d7c2bf47183a429f6795a967ad367784c8f9d0f
Diego Augusto dos Santos <dsdiegods01@gmail.com>
2019-06-12 16:55:58 -0300

    Aula de quarta feira

---
+++ script.js
@@ -0,0 +1,79 @@
+// // // // // let idade = 105;
+// // // // // idade += 1;
+// // // // // //idade = idade + 1;
+
+// // // // // console.log(idade)
+
+// // // // // let nome = 'Lucas';
+// // // // // let cargo = 'programador';
+// // // // // let signo = 'aries';
+
+// alert(`O nome do usuario é:  ${nome}` `e o cargo dele é:  ${cargo}` `e o signo é ${signo}`);
+
+// // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// // // // let materialEscada = prompt("Digite o material da escada");
+// // // // let degrau = materialEscada;
+// // // // let qntd= Number(prompt("opa, quantos degraus ?"));
+
+// // // // for(let i=0; i<qntd; i++){
+// // // //     console.log(materialEscada);
+// // // //     materialEscada = materialEscada + degrau; // Ou += degrau
+// // // // }
+
+
+// // // // let filmes =[
+// // // //     {titulo: "Harry Potter", idade: 10},
+// // // //     {titulo:"It a coisa", idade: 16},
+// // // //     {titulo:"O albergue", idade: 18}
+// // // // ]
+
+// // // // console.log(filmes[0]. ator);
+
+// // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// // // let idd = prompt("Digite sua idade");
+
+// // // let filmes =[
+// // //     {titulo: "Vingadores", idade: 16},
+// // //     {titulo: "Star trek", idade: 17},
+// // //     {titulo: "O exorcista", idade:18}
+// // // ]
+
+// // // for(let i=0; i<filmes.length; i++){
+// // //     console.log(i);
+    
+// // // }
+
+// // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// // function somar(a, b, c){
+// //     console.log(a + b + c);
+// // }
+
+// // somar(10, 1, 5);
+
+// // let val1 = Number(prompt("digite um valor"));
+// // let val2 = Number(prompt("digita outro ae"));
+
+// // function somar(){  // function serve para criar uma função q só se aplica ali dentro e quando vc chamar, nao é global
+// //     console.log(val1 + val2);
+// // }
+
+// // somar();
+
+// // ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// // function calc(a, b,c){
+// //     console.log(a,b,c)
+
+// // }
+
+// // calc(1, '/',3);
+
+// let fazerConta = function(a,b){
+//     console.log(a+b);
+// }
+
